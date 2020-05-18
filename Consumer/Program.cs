3bbcc1658a06c27e3d0fc12608c238be7ffc33aa
Kiran Jagathlal <kiranjagz@gmail.com>
2020-05-18 20:41:57 +0200

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,51 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+using System;
+using System.Threading;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var topic = "test";
+            var cts = new CancellationTokenSource();
+
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                GroupId = "kiran",
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                EnableAutoCommit = false
+            };
+
+            using (var consumer = new ConsumerBuilder<string, string>(config).Build())
+            {
+                consumer.Subscribe(topic);
+             
+                try
+                {
+                    while (true)
+                    {
+                        var consumeResult = consumer.Consume(cts.Token);
+
+                        var blob = JsonConvert.SerializeObject(consumeResult);
+
+                        Console.WriteLine($"Message from kafka: {consumeResult.Message.Value}");
+
+                        consumer.Commit(consumeResult);
+                    }
+                }
+                catch
+                {
+                    //catch bug :P
+                }
+                finally
+                {
+                    consumer.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,36 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Net;
+
+namespace Producer
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            int count = 10;
+            var topic = "test";
+            var key = "kiran";
+
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                ClientId = Dns.GetHostName()
+            };
+
+            using (var producer = new ProducerBuilder<string, string>(config).Build())
+            {
+                for (var i = 0; i <= count; i++)
+                {
+                    await producer.ProduceAsync(topic, new Message<string, string> { Key = key, Value = $"This is a test message with count: {i}" });
+                }
+
+                producer.Flush(TimeSpan.FromSeconds(35));
+            }
+
+            Console.WriteLine($"{count} messages were produced to topic {topic}");
+
+            Console.Read();
+        }
+    }
+}
